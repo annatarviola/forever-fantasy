0a776dc015467330ddd514f6f5888bc740e78934
Lily Springer <lilyspringer@hotmail.com>
2024-09-08 17:40:55 -0600

    work on homepage css

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 
 import Header from "./components/Layout/Header";
+import Home from "./components/Pages/Home.jsx"
 
 function App() {
 
   return (
     <>
       <Header />
+      <Home />
     </>
   );
 }
--- src/components/Pages/Home.jsx
+++ src/components/Pages/Home.jsx
@@ -1,7 +1,23 @@
-import styles from "./Home.module.css"
+import styles from "./Home.module.css";
 
 const Home = () => {
+  return (
+    <>
+      <div>
+        <h1 className={styles.title}>LEVEL UP YOUR EVENT</h1>
+        <h1 className={styles.subtitle1}>
+          with timeless tunes you know and love
+        </h1>
+        <div className={styles.subtitle2}>
+          <span>weddings</span>
+          <span>•</span>
+          <span>birthdays</span>
+          <span>•</span>
+          <span>special events</span>
+        </div>
+      </div>
+    </>
+  );
+};
 
-}
-
-export default Home
+export default Home;
